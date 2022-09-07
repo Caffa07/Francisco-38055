713c58c7c4aa1356c106b268cdebecad1726016c
Caffa <caffa13@gmail.com>
2022-09-06 23:07:03 -0300

    Array

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 alert("Bienvnido a Banco CCF");
 
+
+let input = Number(prompt("Ingresa tu dni"));
+
+while(input === 0){
+    alert("dni no valido");
+    input = Number(prompt("Ingresa tu dni"));
+}
+
 let birthYear = Number(prompt("Ingresar Año de nacimiento"));
 let actualYear = 2022;
 let result = actualYear-birthYear;
 
-
 while(birthYear > 2004 || birthYear < 1950) {
-        alert("Año no Valido");
-        birthYear = Number(prompt("Ingresar Año de nacimiento"));
+            alert("Año no Valido");
+            birthYear = Number(prompt("Ingresar Año de nacimiento"));
+        }
+
+    // Creacion de array de Objetos para usuarios
+    const usuarios = [
+        usiario1 = {
+        dni: 37081504,
+        nombre: "francisoco",
+        apellido: "caffa",
+    },
+    usuario2 = {
+        dni: 35714840,
+        nombre: "raul",
+        apellido: "messi",
+    },
+    usiario3 = {
+        dni: 12255009,
+        nombre: "laura",
+        apellido: "stark",
     }
-   
+    
+]
 
-
-// inicializo las variables a mostrar.
+// Si recorido me da true sigo con los menu de opcionnes
+if (recorido()) {
+    // inicializo las variables a mostrar.
 let saldo = 0;
 let inputOP;
 
@@ -64,9 +91,60 @@ function aprobarPrestamo(){
         alert("La cantidad de cuotas es incorrecta. Puede pedir hasta 24 cuotas.");
         cuotasSolicitadas = Number(prompt("Elija las cantidad de cuotas"));
     }
+    
 
     const result = montoIngresado / cuotasSolicitadas;
     alert("Su credito fue aprobado");
     alert("Te quedan $" + montoIngresado + " en " + cuotasSolicitadas + " cuotas de $" + result);
 }
+}else {
+    alert(`El usuario No exsite.
+            Desea crear un Usuario
+            1. SI
+            2. No
+            0. Salir`);
+        inputOP = Number(prompt("Ingresa una Opcion"));
+        switch (inputOP){
+            case 0:
+                alert("Gracias por su Visita");
+            break;
+            case 1:
+                alert(`Genial vamos a crearle un Usuario `);
+                nuevoUsuario();
+            break;
+            case 2:
+                alert(`No Podra ingresar a CCF`);
+            break;
+            default:
+                alert("Ingrese una opcion valida.");
+            break;
+        }
+}
 
+
+
+
+// Funcion de nuevo usuario
+function nuevoUsuario(dni, name, lastname) {
+    this.dni = dni;
+    this.nombre = name;
+    this.apellido = lastname;
+}
+const usuario4 = new nuevoUsuario(Number(prompt("Ingresa tu dni")), prompt("Ingresa tu nombre"), prompt("ingresa tu apellido"));
+usuarios.push(usuario4);
+
+
+//Funcion para recorer array
+function recorido() {
+    let bandera;
+  for (i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].dni === input) {
+      console.log("Entre");
+        bandera = true;
+      break;
+    } else {
+      break;
+    }
+  }
+  return bandera;
+};
