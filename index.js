46aad47a47319eb3c11ca190188f2909a2e44718
Caffa <caffa13@gmail.com>
2022-09-15 23:08:27 -0300

    Eventos

--- index.js
+++ index.js
@@ -1,95 +1,4 @@
-alert("Bienvnido a Banco CCF");
-
-let input = Number(prompt("Ingresa tu dni"));
-
-while (input === 0) {
-  alert("dni no valido");
-  input = Number(prompt("Ingresa tu dni"));
-}
-
-
-let birthYear = Number(prompt("Ingresar Año de nacimiento"));
-let actualYear = 2022;
-let result = actualYear - birthYear;
-
-while (birthYear > 2004 || birthYear < 1950) {
-  alert("Año no Valido");
-  birthYear = Number(prompt("Ingresar Año de nacimiento"));
-}
-
-// Creacion de array de Objetos para usuarios
-let usuarios = [
-  ({
-    dni: 37081504,
-    nombre: "francisoco",
-    apellido: "caffa",
-  }),
-  ({
-    dni: 35714840,
-    nombre: "raul",
-    apellido: "messi",
-  }),
-  ({
-    dni: 12255009,
-    nombre: "laura",
-    apellido: "stark",
-  }),
-];
-//Le Agrego un filter para que si el Usuario Coincide lo vuelva a saludar
-buscarCliente(input);
-
-getUsuario();
-// inicializo las variables a mostrar.
-let inputOP;
-let saldo = 0;
-
-
-// Si recorido me da true sigo con los menu de opcionnes
-if (recorido()) {
-  
-  // Muestro el menu
-  // Mientras la opcion ingresada no sea cero, muestro resultado
-  // y repito el menu.
-  do {
-    menuPrincipal();
-  } while (inputOP !== 0);
-
-  
-} else {
-  alert(`El usuario No exsite.
-            Desea crear un Usuario
-            1. SI
-            2. No
-            0. Salir`);
-  inputOP = Number(prompt("Ingresa una Opcion"));
-  switch (inputOP) {
-    case 0:
-      alert("Gracias por su Visita");
-      break;
-    case 1:
-      alert(`Genial vamos a crearle un Usuario `);
-      nuevoUsuario();
-      const usuario4 = new nuevoUsuario(
-        Number(prompt("Ingresa tu dni")),
-        prompt("Ingresa tu nombre"),
-        prompt("ingresa tu apellido"),
-    );
-        alert("usuario Creado")
-      usuarios.push(usuario4);
-      console.log(usuario4);
-      do {
-        menuPrincipal();
-      } while (inputOP !== 0);
-      break;
-    case 2:
-      alert(`No Podra ingresar a CCF`);
-      break;
-    default:
-      alert("Ingrese una opcion valida.");
-      break;
-  }
-}
-
+///////////////////////////////////////////////////////////////////////////////////////////////////FUNCIONES/////////////////////////////////////////////////////////////////////////////////////////
 // Funcion de nuevo usuario
 function nuevoUsuario(dni, name, lastname) {
   this.dni = dni;
@@ -180,5 +89,115 @@ function buscarCliente(dni) {
   console.log(usuarios);
 }
 function getUsuario() {
-  usuarios.forEach((usuarios) => alert("Hola! " + usuarios.nombre));
+  const titulo = document.getElementById("titulo");
+  usuarios.forEach((usuarios) => titulo.innerText = "Hola " + usuarios.nombre);
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////FIN FUNCIONES////////////////////////////////////////////////////////////////////////////////////////
+let pepe = document.getElementById("btn");
+pepe.addEventListener("click", () => {
+  getUsuario();
+  
+}) 
+
+let input = Number(prompt("Ingresa tu dni"));
+
+
+
+while (input === 0) {
+  alert("dni no valido");
+  input = Number(prompt("Ingresa tu dni"));
+}
+
+
+let birthYear = Number(prompt("Ingresar Año de nacimiento"));
+let actualYear = 2022;
+let result = actualYear - birthYear;
+
+while (birthYear > 2004 || birthYear < 1950) {
+  alert("Año no Valido");
+  birthYear = Number(prompt("Ingresar Año de nacimiento"));
+}
+
+// Creacion de array de Objetos para usuarios
+let usuarios = [
+  ({
+    dni: 37081504,
+    nombre: "Francisoco",
+    apellido: "caffa",
+  }),
+  ({
+    dni: 35714840,
+    nombre: "Raul",
+    apellido: "messi",
+  }),
+  ({
+    dni: 12255009,
+    nombre: "Laura",
+    apellido: "stark",
+  }),
+];
+//Le Agrego un filter para que si el Usuario Coincide lo vuelva a saludar
+buscarCliente(input);
+
+
+
+
+// getUsuario();
+ 
+
+
+// inicializo las variables a mostrar.
+let inputOP;
+let saldo = 0;
+
+
+// Si recorido me da true sigo con los menu de opcionnes
+if (recorido()) {
+  
+  // Muestro el menu
+  // Mientras la opcion ingresada no sea cero, muestro resultado
+  // y repito el menu.
+  do {
+    menuPrincipal();
+  } while (inputOP !== 0);
+
+  
+} else {
+  alert(`El usuario No exsite.
+            Desea crear un Usuario
+            1. SI
+            2. No
+            0. Salir`);
+  inputOP = Number(prompt("Ingresa una Opcion"));
+  switch (inputOP) {
+    case 0:
+      alert("Gracias por su Visita");
+      break;
+    case 1:
+      alert(`Genial vamos a crearle un Usuario `);
+      nuevoUsuario();
+      const usuario4 = new nuevoUsuario(
+        Number(prompt("Ingresa tu dni")),
+        prompt("Ingresa tu nombre"),
+        prompt("ingresa tu apellido"),
+    );
+        alert("usuario Creado")
+      usuarios.push(usuario4);
+      console.log(usuario4);
+      do {
+        menuPrincipal();
+      } while (inputOP !== 0);
+      break;
+    case 2:
+      alert(`No Podra ingresar a CCF`);
+      break;
+    default:
+      alert("Ingrese una opcion valida.");
+      break;
+  }
+}
+// getUsuario();
+
+
